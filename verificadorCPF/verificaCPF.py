0896bce434fbec5e1c8d5253585a35a157916e9c
Edson Domingos da Silva <e.domingos@academico.ifrn.edu.br>
2022-05-03 09:44:33 -0300

    update codigo verificadorCpf

--- verificadorCPF/verificaCPF.py
+++ verificadorCPF/verificaCPF.py
@@ -3,6 +3,7 @@ from tkinter import *
 
  #Segundo digito verificador
 def digito2(cpf):
+    '''Verifica se o segundo digito verificado do cpf esta valido. Esta função só é executada se o primeiro digito estiver correto.'''
     try:    
         multiplicador = 0
         somador = 0
@@ -11,20 +12,21 @@ def digito2(cpf):
             if multiplicador <= 9:
                 resultado = multiplicador * int(i)
                 somador += resultado
-                multiplicador += 1
-            
+                multiplicador += 1            
         segundoDigito = somador % 11
         if segundoDigito == 10:
             segundoDigito = 0
         if segundoDigito == int(cpf[10]):
             cpfFormatado = f'{cpf[0]}{cpf[1]}{cpf[2]}.{cpf[3]}{cpf[4]}{cpf[5]}.{cpf[6]}{cpf[7]}{cpf[8]}-{cpf[9]}{cpf[10]}'
             resposta['text'] = f'{cpfFormatado}\nNumero  VÁLIDO'
-            return resposta
+        else:
+            resposta['text'] = 'Verifique se os numeros estão corretos'
     except:
         resposta['text'] = 'Verifique se os numeros estão corretos'
 
 
 def digito1(cpf):
+    '''Verifica se o primeiro digito verificado do cpf esta valido.'''
     try:
         multiplicador = 1
         somador = 0
@@ -34,19 +36,18 @@ def digito1(cpf):
                 resultado = multiplicador * int(i)
                 somador += resultado
                 multiplicador += 1
-
         #Pega o resto da divisão da soma por 11 e verifica se o digito verificador é 10
         primeiroDigito = somador % 11
         if primeiroDigito == 10:
             primeiroDigito = 0
         if primeiroDigito == int(cpf[9]):
             digito2(cpf)
-        
+        else:
+            resposta['text'] = 'Verifique se os numeros estão corretos'        
     except:
         if entradaCPF.get() == '':
             resposta['text'] = 'Digite um CPF'
-        else:
-            resposta['text'] = 'Verifique se os numeros estão corretos'
+        
 
 
 
